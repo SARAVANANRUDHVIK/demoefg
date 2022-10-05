68431b1692fde0475149cf31318cd4404a1f1fcc
SARAVANANRUDHVIK <saravana26391@gmail.com>
2022-10-05 13:16:20 +0530

    code second

---
+++ src/test/java/org/test/check.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class check {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.youtube.com/");
+		driver.manage().window().maximize();
+
+	}
+
+}
